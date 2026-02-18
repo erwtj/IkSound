8611d1cde3c8c1cec8b3ee0a947e3af975126a45
Ikno <erwtje013@gmail.com>
2026-02-18 14:26:12 +0100

    Tags with 0 results are now hidden (also audit fix)

--- iksound-vue/src/components/TagItem.vue
+++ iksound-vue/src/components/TagItem.vue
@@ -3,15 +3,15 @@ const props = defineProps({
   tag: {
     displayTag: String,
     slug: String,
+    numTracks: Number
   },
   type: String, 
   isSfx: Boolean
 })
-
 </script>
 
 <template>
-  <li>
+  <li v-if="tag.children.length > 0 || tag.numTracks > 0">
     <RouterLink :to="`/search?${type}=${tag.slug}&sfx=${isSfx}`" class="clickable-link">{{ tag.displayTag }}</RouterLink>
     <ul v-if="tag.children.length">
       <TagItem v-for="child in tag.children" :key="child.id" :tag="child" class="sub-genre" :is-sfx="isSfx" :type="type"></TagItem>
